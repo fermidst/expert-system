d777a188c833e22fadc028349f9478df4d08b144
Mansur Zakirov <mansur.zakirov@boolqa.com>
2020-11-16 00:27:25 +0300

    + normal env base url

--- client-app/src/main.js
+++ client-app/src/main.js
@@ -5,8 +5,11 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 import "vue-datetime/dist/vue-datetime.css";
+import Axios from "axios";
 
 Vue.config.productionTip = false;
+
+Axios.defaults.baseURL = process.env.VUE_BASE_URL || "https://localhost:5000";
 
 new Vue({
   router,
